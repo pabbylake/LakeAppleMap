b521d66741a5dd49fbb3daea53e6c39b986d1eab
PandaLessThan3r <user@classroomlc.local>
2013-10-01 08:58:14 -0400

    d
    d

---
+++ LakeAppleMap/ZS_TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZS_TestViewController.h
+//  LakeAppleMap
+//
+//  Created by Zach Schapiro on 10/1/13.
+//  Copyright (c) 2013 Lake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZS_TestViewController : UIViewController
+
+@end
---
+++ LakeAppleMap/ZS_TestViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZS_TestViewController.m
+//  LakeAppleMap
+//
+//  Created by Zach Schapiro on 10/1/13.
+//  Copyright (c) 2013 Lake. All rights reserved.
+//
+
+#import "ZS_TestViewController.h"
+
+@interface ZS_TestViewController ()
+
+@end
+
+@implementation ZS_TestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
